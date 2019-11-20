588de1891eeb0e7eec2e382b1d4eeef00d1a117c
phuongvu <pvu@uw.edu>
2019-11-19 22:57:46 -0800

    update

--- Visual.R
+++ Visual.R
@@ -4,10 +4,18 @@ library(ggplot2)
 library(plotly)
 
 sleep_data <- read.csv("sleep_data.csv", stringsAsFactors = FALSE)
+polysomnography_studies_df <- read.csv('data/polysomnography_studies.csv',
+                             stringsAsFactors = F)
+actigraphic_studies_df <- read.csv('data/actigraphic_studies.csv',
+                                       stringsAsFactors = F)
 
+polysomnography_grouped_df <- group_by(polysomnography_studies_df, year) %>% 
+  summarize(sleep_time = mean(sleep_time))
+actigraphic_grouped_df <- group_by(polysomnography_studies_df, year) %>% 
+  summarize(sleep_time = mean(sleep_time))
 
-
-
+ggplot(data = polysomnography_grouped_df) + 
+  
 
 plot_ly(
   x = c("Yes", "No"),
