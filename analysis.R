ef8492dd19dd9e341c08c9368bafc9686674c0b0
Hanzhc <hanzhc@uw.edu>
2019-11-19 23:59:54 -0800

    update codes for us map plotting

--- analysis.R
+++ analysis.R
@@ -18,10 +18,31 @@ info_sleep_df <- read.csv("data/demdata_160225_pseudonymized.csv",
 # category: age group, sex. 
 # can see symptoms of sleep deprivation (anxiety, depression, snoring, ), and nap conditions. 
 
-
+# Big data file
 orginal_df <- read.csv("data/500_Cities__Local_Data_for_Better_Health__2018_release.csv", 
                           stringsAsFactors = F)
 us_sleep_deprived <- orginal_df %>% 
   filter(MeasureId == "SLEEP") %>% 
   select(Year, StateAbbr, CityName, Data_Value, PopulationCount, GeoLocation, Short_Question_Text)
 write.csv(us_sleep_deprived, file = "us_sleep_deprived.csv")
+
+# after data wrangling
+us_sleep_deprived <- read.csv("us_sleep_deprived.csv", stringsAsFactors = F)
+# read in dataset with US state and the lat and long
+statelatlong <- read.csv("data/statelatlong.csv", stringsAsFactors = F)
+# data wrangling with dataset, 
+new_df <- group_by(us_sleep_deprived, StateAbbr) %>%
+  filter(StateAbbr != "US") %>% 
+  summarize(ave_percent = sum(Data_Value, na.rm = T) / n(),
+            population = sum(PopulationCount)) %>%
+  mutate(statelatlong$Latitude, statelatlong$Longitude)
+colnames(new_df) <- c("State", "Percent", "Population", "lat", "long")
+
+# create a color numeric scale
+pal <- colorNumeric("Yellow", domain = new_df$Percent)
+# plot the US map with data info
+map <- leaflet(new_df) %>%
+  addTiles() %>% 
+  setView(-98.483330, 38.712046, zoom = 4) %>% 
+  addPolygons(fillColor = ~pal(Percent))
+
