6f4620cfcd677f317dd2964916a15ab0cb5caa6c
Hanzhc <hanzhc@uw.edu>
2019-11-20 00:09:06 -0800

    update code for plot

--- Visual.R
+++ Visual.R
@@ -1,13 +1,15 @@
+install.packages()
 library(dplyr)
 library(stringr)
 library(ggplot2)
 library(plotly)
 
-sleep_data <- read.csv("sleep_data.csv", stringsAsFactors = FALSE)
+sleep_data <- read.csv("data/sleep_data.csv", stringsAsFactors = FALSE)
 polysomnography_studies_df <- read.csv('data/polysomnography_studies.csv',
                              stringsAsFactors = F)
 actigraphic_studies_df <- read.csv('data/actigraphic_studies.csv',
                                        stringsAsFactors = F)
+colnames(polysomnography_studies_df) <- c("year", "age_range", "sleep_time")
 
 polysomnography_grouped_df <- group_by(polysomnography_studies_df, year) %>% 
   summarize(sleep_time = mean(sleep_time))
@@ -15,7 +17,12 @@ actigraphic_grouped_df <- group_by(polysomnography_studies_df, year) %>%
   summarize(sleep_time = mean(sleep_time))
 
 ggplot(data = polysomnography_grouped_df) + 
-  
+  geom_point(mapping = aes(x = year, y = sleep_time)) +
+  geom_smooth(mapping = aes(x = year, y = sleep_time))
+
+ggplot(data = polysomnography_grouped_df) + 
+  geom_point(mapping = aes(x = year, y = sleep_time)) +
+  geom_smooth(mapping = aes(x = year, y = sleep_time))
 
 plot_ly(
   x = c("Yes", "No"),
