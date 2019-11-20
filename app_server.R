8f159f76040f1ad8895740345771782442cae2fb
Hanzhc <hanzhc@uw.edu>
2019-11-20 11:47:18 -0800

    update code

--- app_server.R
+++ app_server.R
@@ -1,5 +1,21 @@
 library(shiny)
+source('analysis.R')
 
 server <- function(input, output) {
-  
+  output$us_map_7 <- renderPlot(
+    plot_usmap(data = new_df, regions = "state",
+               values = "percent", color = "white") +
+      scale_fill_continuous(low = "lightgrey", high = "black", 
+                            name = "Sleep <7 hours(%)",
+                            label = scales::comma) +
+      theme(legend.position = "right") +
+      ggtitle("Among all age group in U.S., 
+Percentage of population getting less than 7 hours sleep")
+  )
+  output$us_timeline <- renderPlot(
+    ggplot(data = grouped_df) + 
+      geom_point(mapping = aes(x = year, y = sleep_time)) +
+      geom_smooth(mapping = aes(x = year, y = sleep_time)) +
+      ggtitle("US adults sleeping times for the recent decades")
+  )
 }
