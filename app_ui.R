b35de481096273118085ff147255a025d3eccda5
Yu-Wen0511 <55999938+Yu-Wen0511@users.noreply.github.com>
2019-12-03 23:41:07 -0800

    Update app_ui.R

--- app_ui.R
+++ app_ui.R
@@ -138,7 +138,7 @@ tabPanel(
                         p("[8] “500 Cities: Local Data for Better Health, 2018 Release.” Centers for Disease Control and Prevention, Centers for Disease Control and Prevention, https://chronicdata.cdc.gov/500-Cities/500-Cities-Local-Data-for-Better-Health-2018-relea/6vp6-wxuq"),
                         p("[9]Monideepa B. Becerra, Brittny S. Bol, Rochelle Granados & Christina Hassija (2018) Sleepless in school: The role of social determinants of sleep health among college students, Journal of American College Health, DOI: 10.1080/07448481.2018.1538148"),
                         p("[10] Life tracking project dataset. Retrieved from: https://www.kaggle.com/maxschmidt94/life-tracking-project-dataset#life_total_data.csv"),
-                        h4("And a special thanks to Andrey Butenko, AKA our wonderful TA who helped us a lot!! :)")
+                        h4("And a special thanks to Andrey Butenko, our wonderful TA who helped us a lot through this course and on this project :)")
                       )
               ),
              tabPanel("About Us",
