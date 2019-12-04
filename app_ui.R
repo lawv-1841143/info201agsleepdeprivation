e4346cc12c5344bf67a9a514a8048009dec2963a
phuongvu <pvu@uw.edu>
2019-12-04 01:51:47 -0800

    update

--- app_ui.R
+++ app_ui.R
@@ -60,10 +60,10 @@ ui <- fluidPage(
           hr()
         ),
         mainPanel(
-          plotOutput("info_sleep_df"),
           tags$div(
             p("Separating by gender allows us to see how both are affected by gender deprivation. Since women and men are so different, the effects can also be different. We will be investigating how men and women feel after a not good night sleep. ")
-          )
+          ),
+          plotOutput("info_sleep_df")
         )
       ),
       
@@ -80,11 +80,11 @@ ui <- fluidPage(
         hr(),
       ),
       mainPanel(
+        tags$div(
+          p("Americans are not getting enough sleep. It also depends how each person perceives what enough sleep is. Many people are tired during the day and cannot focus on their daily activities. By graphing enough and tired, we can see the affects of sleep deprivation.")
+        ),
         plotOutput("sleep_causes_df")
       )
-    ),
-    tags$div(
-      p("Americans are not getting enough sleep. It also depends how each person perceives what enough sleep is. Many people are tired during the day and cannot focus on their daily activities. By graphing enough and tired, we can see the affects of sleep deprivation.")
     )
     ),
   tabPanel(
@@ -140,44 +140,40 @@ ui <- fluidPage(
       h2("Conclusion"),
       p("The strength of our project is that our resources for datasets are from authentic organizations, including American Academy of Sleep Medicine (AASM), National Alliance on Mental Illness (NAMI), and the Centers for Disease Control and Prevention (CDC). By using these credible sources, our group ensures that the information presented in our project is reliable. On the other hand, one of the weaknesses of our project is the narrow age range we covered. We only targeted people aged from 18 to 39, which makes up about 26.3 percent of the U.S population. he project could be more applicable to more users. The main lesson our team learned from this project is, the sleeping trend in the U.S in recent years is getting worse. We found out that the lack of sleep could affect not only students' academic performance but also human health. In the future, people could improve the project by extending the age group and provide suggestions to improve people’s sleeping time and sleeping quality. Our team would also like to look deeper into the sleeping trend in different countries other than the U.S to make a comparison, as well as how foreign governments approach sleeping issues.")
     )
+  ),
+  navbarMenu(
+    "About",
+    tabPanel(
+      "About Tech",
+      titlePanel("Give credit to all the amazing sources!"),
+      tags$div(
+        p("[1] Cunningham, J. (2019). College students aren’t getting nearly enough sleep. Retrieved from http://sleepeducation.org/news/2019/07/18/college-students-are-not-getting-nearly-enough-sleep"),
+        p("[2] Feraco, F. (2018). Sleep Deprivation. Retrieved from https://www.kaggle.com/feraco/sleep-deprivation#demdata_160225_pseudonymized.csv"),
+        p("[3] Fusion 360. (2014). Sleepless Nights. Retrieved from https://visual.ly/community/infographic/health/sleepless-nights"),
+        p("[4] Healthguru. (2012). Need More Sleep? The Facts On Sleeping Disorders. Retrieved from https://visual.ly/community/infographic/health/need-more-sleep-facts-sleeping-disorders"),
+        p("[5] Lomuscio, M. (2019). Sleep Study. Retrieved from https://www.kaggle.com/mlomuscio/sleepstudypilot"),
+        p("[6] Mental Health Guide for College Students. (2019). Retrieved from https://collegestats.org/resources/mental-health-guide/"),
+        p("[7] Youngstedt, Shawn D et al. “Has adult sleep duration declined over the last 50+ years?.” Sleep medicine reviews vol. 28 (2016): 69-85. doi:10.1016/j.smrv.2015.08.004"),
+        p("[8] “500 Cities: Local Data for Better Health, 2018 Release.” Centers for Disease Control and Prevention, Centers for Disease Control and Prevention, https://chronicdata.cdc.gov/500-Cities/500-Cities-Local-Data-for-Better-Health-2018-relea/6vp6-wxuq"),
+        p("[9]Monideepa B. Becerra, Brittny S. Bol, Rochelle Granados & Christina Hassija (2018) Sleepless in school: The role of social determinants of sleep health among college students, Journal of American College Health, DOI: 10.1080/07448481.2018.1538148"),
+        p("[10] Life tracking project dataset. Retrieved from: https://www.kaggle.com/maxschmidt94/life-tracking-project-dataset#life_total_data.csv"),
+        h4("And a special thanks to Andrey Butenko, our wonderful TA who helped us a lot through this course and on this project :) ")
+      )
+    ),
+    tabPanel(
+      "About Us",
+      titlePanel("More Information on project members!"),
+      tags$div(
+        h3("Phuong Vu"),
+        p("Phuong Vu is an international student at the University of Washington who wants to study Informatics, and this is his second year at the UW. He enjoys writing code that would solve real-life tasks. During his free time, he loves traveling to new places to take artistic photos and creating videos."),
+        h3("Yu-Wen Chen"),
+        p("Yu-Wen Chen is currently a Freshman at the University of Washington from Taoyuan, Taiwan. She enjoys creative problem solving and figuring things out with her team. Outside of  the classroom, she loves spending time doing creative writing and reading Asian literature. Most importantly, she thinks her dog May-May is the cutest dog in the universe."),
+        h3("Hanzhi Cao"),
+        p("Hanzhi Cao is an international student at the UW studying Psychology in her senior year. On one hand she loves psychology and would love to know more about the mysterious human kind. On the other hand, she is also into data field that people are generating data every day every second. She believes that efficiency is EVERYTHING, so her ultimate goal of life is to find a better way to improve human life experience."),
+        h3("Vivian Law"),
+        p("Vivian Law is a junior student at the University of Washington. She is in the Early Childhood Family Studies major. She enjoys photography and trying different foods. She has a passion for children and for technology. She values her Taiwanese and Cantonese culture.")
+      )
+    )
   )
-  # tabPanel(
-  # navbarMenu(
-  #   "About",
-  #   tabPanel(
-  #     "About Tech",
-  #     # titlePanel("Give credit to all the amazing sources!")
-  #     tags$div(
-  #       h2("Give credit to all the amazing sources!"),
-  #       p("[1] Cunningham, J. (2019). College students aren’t getting nearly enough sleep. Retrieved from http://sleepeducation.org/news/2019/07/18/college-students-are-not-getting-nearly-enough-sleep"),
-  #       p("[2] Feraco, F. (2018). Sleep Deprivation. Retrieved from https://www.kaggle.com/feraco/sleep-deprivation#demdata_160225_pseudonymized.csv"),
-  #       p("[3] Fusion 360. (2014). Sleepless Nights. Retrieved from https://visual.ly/community/infographic/health/sleepless-nights"),
-  #       p("[4] Healthguru. (2012). Need More Sleep? The Facts On Sleeping Disorders. Retrieved from https://visual.ly/community/infographic/health/need-more-sleep-facts-sleeping-disorders"),
-  #       p("[5] Lomuscio, M. (2019). Sleep Study. Retrieved from https://www.kaggle.com/mlomuscio/sleepstudypilot"),
-  #       p("[6] Mental Health Guide for College Students. (2019). Retrieved from https://collegestats.org/resources/mental-health-guide/"),
-  #       p("[7] Youngstedt, Shawn D et al. “Has adult sleep duration declined over the last 50+ years?.” Sleep medicine reviews vol. 28 (2016): 69-85. doi:10.1016/j.smrv.2015.08.004"),
-  #       p("[8] “500 Cities: Local Data for Better Health, 2018 Release.” Centers for Disease Control and Prevention, Centers for Disease Control and Prevention, https://chronicdata.cdc.gov/500-Cities/500-Cities-Local-Data-for-Better-Health-2018-relea/6vp6-wxuq"),
-  #       p("[9]Monideepa B. Becerra, Brittny S. Bol, Rochelle Granados & Christina Hassija (2018) Sleepless in school: The role of social determinants of sleep health among college students, Journal of American College Health, DOI: 10.1080/07448481.2018.1538148"),
-  #       p("[10] Life tracking project dataset. Retrieved from: https://www.kaggle.com/maxschmidt94/life-tracking-project-dataset#life_total_data.csv"),
-  #       h4("And a special thanks to Andrey Butenko, our wonderful TA who helped us a lot through this course and on this project :) ")
-  #     )
-  # ),
-  # tabPanel(
-  #   "About Us",
-  #   titlePanel("More Information on project members!"),
-  #   tags$div(
-  #     h2("More Information on project members!"),
-  #     h3("Phuong Vu"),
-  #     p("Phuong Vu is an international student at the University of Washington who wants to study Informatics, and this is his second year at the UW. He enjoys writing code that would solve real-life tasks. During his free time, he loves traveling to new places to take artistic photos and creating videos."),
-  #     h3("Yu-Wen Chen"),
-  #     p("Yu-Wen Chen is currently a Freshman at the University of Washington from Taoyuan, Taiwan. She enjoys creative problem solving and figuring things out with her team. Outside of  the classroom, she loves spending time doing creative writing and reading Asian literature. Most importantly, she thinks her dog May-May is the cutest dog in the universe."),
-  #     h3("Hanzhi Cao"),
-  #     p("Hanzhi Cao is an international student at the UW studying Psychology in her senior year. On one hand she loves psychology and would love to know more about the mysterious human kind. On the other hand, she is also into data field that people are generating data every day every second. She believes that efficiency is EVERYTHING, so her ultimate goal of life is to find a better way to improve human life experience."),
-  #     h3("Vivian Law"),
-  #     p("Vivian Law is a junior student at the University of Washington. She is in the Early Childhood Family Studies major. She enjoys photography and trying different foods. She has a passion for children and for technology. She values her Taiwanese and Cantonese culture. ")
-  #   )
-  # )
-  # )
-  # )
 )
 )
