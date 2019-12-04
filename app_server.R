04e245fd2ec47f668b4c083808230ccafbef7a57
phuongvu <pvu@uw.edu>
2019-12-04 00:04:21 -0800

    update

--- app_server.R
+++ app_server.R
@@ -10,12 +10,10 @@ server <- function(input, output) {
   output$us_timeline <- renderPlot(
     draw_sleep_trend_plot(input$StudiesMethods)
   )
-  #output$value <- renderPrint({input$YearRange})
   output$compared.bar <- renderPlotly(
     draw_compare_bar(input$select.activities)
   )
   output$sleep_GPA <- renderPlotly({
     return(draw_bar_graph_gpa_tired())
-    
   })
 }
